14296e4aa799d62450c620eff17f69548452e9e0
Pedro Henrique <1pedrohenri@gmail.com>
2024-12-21 03:52:51 -0300

    ✨ feat: loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,20 @@
+export function Loading() {
+  return (
+    <div className="flex flex-col items-center justify-center h-[50vh] bg-transparent">
+      <div className="relative flex items-center space-x-4">
+        <div className="relative w-2 h-16 bg-sithRed rounded animate-pulse">
+          <div className="absolute inset-0 h-full w-full blur-lg bg-sithRed opacity-50"></div>
+        </div>
+        <div className="relative w-2 h-16 bg-jediBlue rounded animate-pulse delay-200">
+          <div className="absolute inset-0 h-full w-full blur-lg bg-jediBlue opacity-50"></div>
+        </div>
+        <div className="relative w-2 h-16 bg-lightsaberGreen rounded animate-pulse delay-400">
+          <div className="absolute inset-0 h-full w-full blur-lg bg-lightsaberGreen opacity-50"></div>
+        </div>
+      </div>
+      <p className="mt-6 text-droidGold text-lg font-semibold tracking-wider star-wars-text">
+        Loading...
+      </p>
+    </div>
+  );
+};
