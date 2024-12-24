21a0519a0fc4fccc4708bb4f23c97fd755ce1032
Pedro Henrique <1pedrohenri@gmail.com>
2024-12-24 09:38:10 -0300

    💄 style: change loading component to rick and morty like instead star wars like

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,18 +1,10 @@
 export function Loading() {
   return (
     <div className="flex flex-col items-center justify-center h-[70vh] bg-transparent">
-      <div className="relative flex items-center space-x-4">
-        <div className="relative w-2 h-16 bg-sithRed rounded animate-pulse">
-          <div className="absolute inset-0 h-full w-full blur-lg bg-sithRed opacity-50"></div>
-        </div>
-        <div className="relative w-2 h-16 bg-jediBlue rounded animate-pulse delay-200">
-          <div className="absolute inset-0 h-full w-full blur-lg bg-jediBlue opacity-50"></div>
-        </div>
-        <div className="relative w-2 h-16 bg-lightsaberGreen rounded animate-pulse delay-400">
-          <div className="absolute inset-0 h-full w-full blur-lg bg-lightsaberGreen opacity-50"></div>
-        </div>
+      <div className="relative flex items-center">
+        <div className="portal-animation w-20 h-20"></div>
       </div>
-      <p className="mt-6 text-droidGold text-lg font-semibold tracking-wider star-wars-text">
+      <p className="mt-6 text-green-500 text-lg font-semibold tracking-wider rick-morty-text">
         Loading...
       </p>
     </div>
