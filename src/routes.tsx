3f09c9b34d42d9aa05e6de73282965559038ae68
Pedro Henrique <1pedrohenri@gmail.com>
2024-12-27 09:21:12 -0300

    ⏪ revert: 404 page This reverts commit 44b504a1d3ee3394d66390c2497e7ef9b5131a89.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,9 @@
 import { Router, Route } from 'wouter';
 import {HomePage, FavoritesPage} from "@/pages"
-import { NotFound } from '@/components/NotFound';
 
 export const Routes = () => (
   <Router>
     <Route path="/" component={HomePage} />
     <Route path="/favorites" component={FavoritesPage} />
-    <Route path="*" component={NotFound} />
   </Router>
 );
